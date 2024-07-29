313ee4bf12fa4d598a8f54b6886be33c896e96df
schneemaier <akos.schneemaier@gmail.com>
2024-07-29 17:11:27 -0400

    Removed unused files from Openmiko

--- src/api/README.md
+++
@@ -1,1 +0,0 @@
-The OpenMiko `api` binary.
--- src/db/db.h
+++
@@ -1,6 +0,0 @@
-#include <sqlite3.h>
-#include <models.h>
-
-void open_database();
-void get_user(char* email, User *user);
-void generate_session_token(User *user, char* session_token);
